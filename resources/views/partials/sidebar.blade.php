bf0804a33af7668085a050d0a266f6ba348817fb
andixanrivi09 <andixanrivi09@gmail.com>
2024-07-26 09:58:31 +0700

    fix : add function active helper sidebar menu active

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
     $role = 'superadmin';
 @endphp
 
-<nav class="col-md-2 d-none d-md-block sidebar fixed-sidebar" id="sidebar">
+<nav class="col-md-2 sidebar fixed-sidebar" id="sidebar">
     <div class="sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item text-center">
@@ -19,43 +19,41 @@
             </li>
             @if ($role == 'superadmin')
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">
+                    <a class="nav-link {{ setActiveClass(['home']) || (request()->routeIs('club-pending') && !request()->routeIs('clubs-index')) ? 'active' : '' }}"
+                        href="{{ route('home') }}">
                         <img src="{{ asset('assets/template/icon/Home.svg') }}" alt="Home" class="nav-icon">
                         <span>Home</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('clubs-index', 'club-create', 'club-detail', 'event-create') ? 'active' : '' }}"
+                    <a class="nav-link {{ setActiveClass(['clubs-index', 'club-create', 'club-detail', 'event-create']) || (request()->routeIs('club-pending') && request()->routeIs('clubs-index')) ? 'active' : '' }}"
                         href="{{ route('clubs-index') }}">
                         <img src="{{ asset('assets/template/icon/Club.svg') }}" alt="All Clubs" class="nav-icon">
                         <span>All Clubs</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('users-list') ? 'active' : '' }}"
-                        href="{{ route('users-list') }}">
+                    <a class="nav-link {{ setActiveClass(['users-list']) }}" href="{{ route('users-list') }}">
                         <img src="{{ asset('assets/template/icon/Users.svg') }}" alt="Users List" class="nav-icon">
                         <span>Users List</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('admins-list', 'admins-create') ? 'active' : '' }}"
+                    <a class="nav-link {{ setActiveClass(['admins-list', 'admins-create']) }}"
                         href="{{ route('admins-list') }}">
                         <img src="{{ asset('assets/template/icon/UsersList.svg') }}" alt="All Admins" class="nav-icon">
                         <span>All Admins</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('notifications') ? 'active' : '' }}"
-                        href="{{ route('notifications') }}">
+                    <a class="nav-link {{ setActiveClass(['notifications']) }}" href="{{ route('notifications') }}">
                         <img src="{{ asset('assets/template/icon/Notification.svg') }}" alt="Notifications"
                             class="nav-icon">
                         <span>Notifications</span>
                     </a>
                 </li>
                 <li class="nav-item settings">
-                    <a class="nav-link {{ request()->routeIs('settings') ? 'active' : '' }}"
-                        href="{{ route('settings') }}">
+                    <a class="nav-link {{ setActiveClass(['settings']) }}" href="{{ route('settings') }}">
                         <img src="{{ asset('assets/template/icon/Setting.svg') }}" alt="Settings" class="nav-icon">
                         <span>Settings</span>
                     </a>
