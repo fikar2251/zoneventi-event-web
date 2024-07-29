8e4b80194cb87de10544bba4d62a3a99fcee4acc
Zulfikar Jafar <zulfikarjs24@gmail.com>
2024-07-29 09:56:12 +0700

    fix: add role to users_detail web

--- app/Http/Controllers/Web/Admin/AuthController.php
+++ app/Http/Controllers/Web/Admin/AuthController.php
@@ -102,6 +102,7 @@ class AuthController extends Controller
             'document_number' => $request->document_number,
             'documents_expire_date' => $request->expire_date,
             'documents_file' => $picUrl,
+            'role' => 'owner'
         ]);
 
         return redirect('login')->with('success', 'Registered Successfully');
